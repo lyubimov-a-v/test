64fba0ae263b1a9d4f5b891b08b2933e04d77fc5
lyubimov-a-v <aleksey.lyubimov@gmail.com>
2014-09-24 08:47:52 +1000

    Second

--- Test/Program.cs
+++ Test/Program.cs
@@ -10,7 +10,7 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello,world!");
+            Console.WriteLine("Hello,world, you are done!");
         }
     }
 }
